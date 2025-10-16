f97e8bfbf362f1949e1955bc8e6092ab78ca310f
julianaq19 <j24492377@gmail.com>
2025-10-16 15:38:54 -0500

    taller empezado

--- Estructura_de_control/Program.cs
+++ Estructura_de_control/Program.cs
@@ -47,20 +47,58 @@ namespace Estructura_de_control
             //}
 
             //ejercicio3
-            Console.WriteLine("Ingrese el precio del producto:");
-            double precio = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese el precio del producto:");
+            //double precio = double.Parse(Console.ReadLine());
 
-            double precioFinal;
+            //double precioFinal;
 
-            if(precio >= 100000)
-            {
-                precioFinal = precio - (precio * 0.2); // Aplica un descuento del 10%
-            }
-            else
-            {
-                precioFinal = precio; // No hay descuento
-            }
-            Console.WriteLine("El precio final es: " + precioFinal);
+            //if(precio >= 100000)
+            //{
+            //    precioFinal = precio - (precio * 0.2); // Aplica un descuento del 10%
+            //}
+            //else
+            //{
+            //    precioFinal = precio; // No hay descuento
+            //}
+            //Console.WriteLine("El precio final es: " + precioFinal);
+
+
+            //TALLER ESTRUCTURA DE CONTROL
+            //ejercicio1
+            //    Console.WriteLine("Ingrese el monto del prestamo: ");
+            //    double monto = double.Parse(Console.ReadLine());
+            //    double tasa = 0.05;
+            //    double año = 5;
+
+            //    double interesAnual = monto * tasa;
+            //    double interesTrimestre3 = interesAnual /4;
+
+            //    double interesPrimerMes = interesAnual / 12;
+            //    double totalConInteres = monto + (interesAnual * año);
+
+            //    Console.WriteLine("interes en un año: " + interesAnual);
+            //    Console.WriteLine("Interes en el tercer trimestre: "+ interesTrimestre3);
+            //    Console.WriteLine("Interes en el primer mes: " + interesPrimerMes);
+            //    Console.WriteLine("Total a pagar (5 años): "+  totalConInteres);
+
+            //ejercicio2
+            //Console.WriteLine("Salario del empleado: ");
+            //double salario = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Valor de ahorro mensual: ");
+            //double  ahorro = double.Parse(Console.ReadLine());
+
+            //double salud = salario * 0.125;
+            //double pension = salario * 0.16;
+            //double total = salario - (salud + pension + ahorro);
+
+            //Console.WriteLine("\n--- Colilla de Pago ---");
+            //Console.WriteLine("Salario:" + salario);
+            //Console.WriteLine("Ahorro mensual: " + ahorro);
+            //Console.WriteLine("Descuento pension (16%): " + pension);
+            //Console.WriteLine("Total a recibir: " + total);
+
+            // ejercicio3
         }
     }
 }
